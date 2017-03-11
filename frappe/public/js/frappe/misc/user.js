9ab833f2d517089e0b36e517db7f0cff5b514c08
elba7r <elba7r@gmail.com>
2017-03-11 17:44:56 +0200

    interface-changes-lightblue

--- frappe/public/js/frappe/misc/user.js
+++ frappe/public/js/frappe/misc/user.js
@@ -38,7 +38,7 @@ frappe.ui.set_user_background = function(src, selector, style) {
 		}
 		var background = repl('background: url("%(src)s") center center;', {src: src});
 	} else {
-		var background = "background-color: #919191;";
+		var background = "background-color: #C4C4C4;";
 	}
 
 	frappe.dom.set_style(repl('%(selector)s { \
